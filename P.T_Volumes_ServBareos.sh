f392630d21cc35181683c44000226c2a6ca6854d
luan-albuquerque <luan.santos6605@gmail.com>
2021-02-10 20:37:16 -0400

    Iniciando codigo de para automatização de exclusão de volumes no Bareos(Servidor de Backup)

---
+++ P.T_Volumes_ServBareos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#################################################
+#
+#     Console Script Purge and Truncate for Volumes
+#     Escrito por: Luan Albuquerque dos Santos
+#     Last Update: 27/10/2020
+#
+#################################################
+
